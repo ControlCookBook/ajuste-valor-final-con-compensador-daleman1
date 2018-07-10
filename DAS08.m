5827a5d654be524124857cb25ccb4849fa527842
daleman1 <39544932+daleman1@users.noreply.github.com>
2018-07-10 17:06:23 -0600

    tarea #8 Daniel Aleman Saborio

---
+++ DAS08.m
@@ -0,0 +1,14 @@
+% Tarea #8 Daniel Aleman saborio 
+clear all
+clc
+G=tf([1],[1 10 24])
+GC=feedback(G,1)
+num=[5]
+den=[1 10 24]
+%Respuesta a un escalon 
+c=tf([num],[1])
+H=series(c,G)
+FC=feedback(H,1)
+step(FC)
+grid
+title('Curva ante un escalon')
